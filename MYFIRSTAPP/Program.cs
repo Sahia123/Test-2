8a01bcfd70665479a2b18cdeb968b3c5d7611196
Sahia123 <sahiatech123@gmail.com>
2021-12-14 23:14:30 +0500

    This is my 1st commit

---
+++ MYFIRSTAPP/Program.cs
@@ -0,0 +1,28 @@
+﻿using System; //framwork class ibraries---> classes and methods--> here using: keyword sys:ns
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYFIRSTAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Console.Write("Asalam o Alaikum");
+              Console.WriteLine("Learning C#");*/
+
+            int age ;
+            string name;
+            Console.WriteLine("Enter your name:"); //for printing on the screen
+            name = Console.ReadLine(); // for taking input it is in string format
+            Console.WriteLine("Enter your age:");
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello " + name);
+            Console.Write("You are " + age + " years old");
+            Console.Read();
+        }
+    }
+}
